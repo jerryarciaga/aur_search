b797813ecb153b2780705ec07688545c6319cdb7
Jerry Arciaga <jerryarciaga11@gmail.com>
2020-11-10 12:29:06 -0700

    Added package class to contain AUR packages

---
+++ aur_package.py
@@ -0,0 +1,12 @@
+class AUR_Package:
+    name = ''
+    link = ''
+    desc = ''
+
+    def __init__(self, name, link, desc):
+        self.name = name
+        self.link = link
+        self.desc = desc
+
+    def __str__(self):
+        return f'{self.name} <{self.link}>\n\t{self.desc}'
