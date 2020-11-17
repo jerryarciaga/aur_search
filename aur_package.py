f998186d26c1bfcedfa822c33cbc1312ebf554d8
Jerry Arciaga <jerryarciaga11@gmail.com>
2020-11-17 12:19:34 -0700

    Added Version tag to search results

--- aur_package.py
+++ aur_package.py
@@ -3,13 +3,14 @@ class AUR_Package:
     link = ''
     desc = ''
 
-    def __init__(self, name, link, desc):
+    def __init__(self, name, ver, link, desc):
         self.name = name
+        self.ver  = ver
         self.link = link
         self.desc = desc
 
     def __str__(self):
-        return f'{self.name} <{self.link}>\n\t{self.desc}'
+        return f'aur/{self.name} {self.ver} <{self.link}> \n\t{self.desc}'
 
 if __name__ == '__main__':
     pass
