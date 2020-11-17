8b6b79475bbbb6df0dddd7121c92a2157ddc21aa
Jerry Arciaga <jerryarciaga11@gmail.com>
2020-11-17 12:20:39 -0700

    Abandoned web-scraping; used AUR RPC to gather search results [Complete]

--- search_aur.py
+++ search_aur.py
@@ -1,7 +1,6 @@
 import sys
 import requests
 import json
-from bs4 import BeautifulSoup
 
 from aur_package import AUR_Package
 
@@ -10,28 +9,26 @@ if __name__ == '__main__':
         print('Usage: search_aur [PACKAGE_NAME]')
     else:
         # Set headers and request content
-        url = 'https://aur.archlinux.org/packages'
+        url = 'https://aur.archlinux.org/rpc/'
         headers = {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'
         }
         params = {
-            'O': 0,
-            'SeB': 'nd',
-            'K': sys.argv[1],
-            'SB': 'n',
-            'SO': 'a',
-            'PP': 250,
-            'do_Search': 'Go',
+            'v': 5,
+            'type': 'search',
+            'by': 'name',
+            'arg': sys.argv[1],
         }
         
         # Look up AUR Package
         aur_search = requests.get(url=url, headers=headers, params=params)
-
-        # Parse search results
-        aur_results = BeautifulSoup(aur_search.text, 'html.parser')
-        aur_packages = []
         
-        odd_row = '#pkglist-results-form > table > tbody > tr.odd > td:nth-child(1) > a'
-        evn_row = '#pkglist-results-form > table > tbody > tr.even > td:nth-child(1) > a'
-        print(aur_results.select(odd_row))
-        print(aur_results.select(evn_row))
+        # Load each result to AUR_Package class
+        for result in json.loads(aur_search.text)['results']:
+            package = AUR_Package(
+                name = result['Name'],
+                ver = result['Version'],
+                link = result['URL'],
+                desc = result['Description'],
+                )
+            print(package)
