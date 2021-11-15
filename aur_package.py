e2c0c2b05a9515320ee7432edd6482c37b2ac6d6
Jerry Arciaga <jerryarciaga11@gmail.com>
2021-11-15 04:24:55 -0700

    Added Clone URL to easily clone AUR Package

--- aur_package.py
+++ aur_package.py
@@ -1,16 +1,20 @@
 class AUR_Package:
     name = ''
+    clone_url = ''
     link = ''
     desc = ''
 
     def __init__(self, name, ver, link, desc):
         self.name = name
+        self.clone_url = "https://aur.archlinux.org/" + self.name + ".git"
         self.ver  = ver
         self.link = link
         self.desc = desc
 
     def __str__(self):
-        return f'aur/{self.name} {self.ver} <{self.link}> \n\t{self.desc}'
+        return f'''aur/{self.name} {self.ver} <{self.link}>
+        Clone URL: {self.clone_url}
+        {self.desc}'''
 
 if __name__ == '__main__':
     pass
